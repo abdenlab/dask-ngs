fb6ee72336a6ddecf9ba5c488dce8882152c9889
Jonathan Joyce <jonmjoyce@gmail.com>
2023-07-21 15:14:00 -0600

    Implemented test_chunk_offsets test

--- tests/test_chunk_indexes.py
+++ tests/test_chunk_indexes.py
@@ -19,6 +19,23 @@ def offsets(example_bai):
     # TODO: Not sure if multiple BAI indexes should be tested
     return bai[0]["ioffsets"]
 
-def test_chunk(offsets):
-    offsets_uniq = _index.chunk_offsets(offsets, 1_000_000)
-    assert True
+def test_chunk_offsets(offsets):
+    chunksize = 1_000_000
+    offsets_uniq = _index.chunk_offsets(offsets, chunksize)
+    
+    cumsum = 0
+    for i in range(len(offsets_uniq)-1):
+        prev = offsets_uniq.iloc[i]
+        next = offsets_uniq.iloc[i+1]
+        # validate differences
+        assert next['ioffset.cpos'] - prev['ioffset.cpos'] == next['ioffset.cpos.diff']
+        cumsum += next['ioffset.cpos.diff']
+        if cumsum > chunksize:
+            cumsum = 0
+        # validate chunk sizes
+        assert next['size'] == cumsum
+        assert next['size'] <= chunksize
+    return offsets_uniq
+
+#def test_chunk_groups(test_chunk_offsets):
+#    offset_groups = _index.group_chunks(test_chunk_offsets)
